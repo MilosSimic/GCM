8213f9622b901a6a132437768064f24042b46b33
milossimic <milossimicsimo@gmail.com>
2015-12-15 12:03:57 +0100

    Test files

--- python/test.py
+++ python/test.py
@@ -1,13 +1,13 @@
 from gcm import GCM
 
-SERVER_GCM_KEY = "app_id"
-CLIENT_GCM = ["client_id"]
+SERVER_GCM_KEY = "AIzaSyD5CwBRQLMS0fuXh99T-t846FDmJbnA_G8"
+CLIENT_GCM = ["dCS3kAB1CB4:APA91bEkUfR0xn0FeCnmyZZvl7OM55r3xh3XpoiAzw_-5g58e-RIag5YcjYqyKgLX8LQQu1ofBXKx5FkqL2DG0d_ASgdTodLPH1gvugyLXiTj7B77QPgEdwPun6hT7IeocyA4TciLfut"]
 
 data = {"msgid":102,
 			"sender":"Ydn",
 			"send_date": "2014-08-15 13:26:13",
 			"subject":"haee kak lupeks",
-			"message":"halooo kak lupeks... kira2 ya gini sih hasilnyaaa.... :p ",
+			"message":"Hello world",
 			"extras":{"idJob":1, "idTicket":10, "ticket_no":"BCA_IN_AAAB"}}
 
 gcm_notify = GCM(SERVER_GCM_KEY)
